4a47116a3ab94e1e18e18af7555e126fc5259e40
fatihlocos <fatih_ozer@live.be>
2022-02-10 15:48:12 +0100

    string methods functions objects

--- JAVASCRIPT/tutorial6.js
+++ JAVASCRIPT/tutorial6.js
@@ -1,27 +1,74 @@
 // event
 console.log('hello')
-let mybtn = document.getElementById("MyButton")
+let mybtn = document.getElementById("myButton")
 let myinput = document.getElementById('username')
+let result = document.getElementById('result')
 console.log(mybtn)
 
 
-function Clickhandler(){
+function ClickHandler(){
     console.log('click event!!!')
     console.log(myinput.value)
     console.log(result)
     result.innerText = myinput.value
 }
 
-mybtn.addEventListener('click',Clickhandler)
 
+mybtn.addEventListener('click',ClickHandler)
 
-let_result;
-let text = "Lorem lorem lorem"
-
-
-
+let _result;
+let text = "Lorem ipsum dolor sit amet intecbrussel consectetur adipisicing elit. Expedita laboriosam fugit est intecbrussel vitae deserunt"
+let example = "23627323 237232873 sadhjasdjasjdajhd 237283 INTECBRUSSEL hsdjhsdjhsdj 7323 sjdhhsdjdhsj"
+let url = '               http://google.com"><script>alert(1)</script>                    '
 // string methods
 
-// length
+// length 
 _result = text.length
-console.log(_result)
+
+// indexOf
+_result = text.indexOf("intecbrussel")
+_result = text.lastIndexOf("intecbrussel")
+
+// search method
+_result = text.search("amet")
+
+// extract string
+_result = text.slice(6,11)
+_result = url.slice(0,17)
+console.log(text.length-15)
+_result = text.substring(5,text.length-15)
+
+// replace method
+_result = text.replace("amet","google")
+_result = example.replace(/([0-9])/gm,'...')
+_result = example.replace(/([a-zA-Z])/gm,'...')
+
+// uppercase lowercase methods
+_result = text.toUpperCase();
+_result = text.toLowerCase();
+
+_result = url
+
+// remove spacing (trim)
+_reuslt = url.trim();
+_result  = url.trimLeft();
+_result = url.trimRight();
+
+var name = "intec"
+var brx = "brussel"
+// name +="brussel"
+// _result = name
+
+// concat method
+_result = name.concat(brx)
+
+
+var students = "Fatih-AbdulJelil-Prince"
+
+_result = students
+
+// convert string to array split method
+
+_result = students.split("-")[3]
+
+console.log(_result, typeof _result)
